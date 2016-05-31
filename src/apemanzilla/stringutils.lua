4e904e91153e8778045671e60ef84411207d77dc
apemanzilla <apemanzilla@users.noreply.github.com>
2016-05-31 14:06:52 -0400

    Add stringutils.fixLength

--- src/apemanzilla/stringutils.lua
+++ src/apemanzilla/stringutils.lua
@@ -30,6 +30,26 @@ function stringutils.wrapLines(text, width)
 	return lines
 end
 
-stringutils.longtext = "this is a really long string, i hope it gets wrapped properly.\nit also has a couple\n\nnew\nlines"
+function stringutils.fixLength(text, length, pad)
+	if type(text) ~= "string" and type(text) ~= "table" then
+		return error("expected string/table, got " .. type(text) .. " for arg 1")
+	end
+	expect(length, "number", 2)
+	local pad = expect(pad, "string", 3, true) or " "
+
+	local function padGen(n)
+		return pad:rep(math.floor(n / #pad)) .. pad:sub(1, n % #pad)
+	end
+
+	if type(text) == "string" then
+		return text:sub(1, length) .. padGen(math.max(0, length - #text))
+	else
+		local out = {}
+		for i, line in ipairs(text) do
+			out[i] = stringutils.fixLength(line, length, pad)
+		end
+		return out
+	end
+end
 
 return stringutils
