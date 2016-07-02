88b6f3b01f042be02928dea0ef48bdd5c29e7d0a
apemanzilla <dominic@marcuse.us>
2016-07-02 17:43:27 -0400

    Patch to allow charlist to start at char 1

--- src/apemanzilla/charlist.lua
+++ src/apemanzilla/charlist.lua
@@ -3,8 +3,8 @@ local pager = require "pager"
 
 local str = ""
 
-for i = 32, 255 do
-	str = str .. stringutils.fixLength(i .. ": " .. string.char(i), 6) .. (((i + 4) % 7 == 0 and "\n") or " ")
+for i = 1, 255 do
+	str = str .. stringutils.fixLength(i .. ": " .. ((i == 10 and " ") or string.char(i)), 6) .. ((i % 7 == 0 and "\n") or " ")
 end
 
-pager.scroll(str .. "\n")
+pager.scroll(str .. "\n\n")
