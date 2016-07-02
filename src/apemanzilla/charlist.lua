c8bf75c1943c8753ceef3b40a2e418a5916cc891
apemanzilla <dominic@marcuse.us>
2016-07-02 01:21:38 -0400

    Minor fix for charlist package

--- src/apemanzilla/charlist.lua
+++ src/apemanzilla/charlist.lua
@@ -4,7 +4,7 @@ local pager = require "pager"
 local str = ""
 
 for i = 32, 255 do
-	str = str .. stringutils.fixLength(i .. ": " .. string.char(i), 6) .. ((i % 7 == 0 and "\n") or " ")
+	str = str .. stringutils.fixLength(i .. ": " .. string.char(i), 6) .. (((i + 4) % 7 == 0 and "\n") or " ")
 end
 
-pager.scroll(str .. "\n\n")
+pager.scroll(str .. "\n")
