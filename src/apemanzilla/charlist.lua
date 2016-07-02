27d487f3a2f586ef6bfb71e1993f68798da8f94f
apemanzilla <dominic@marcuse.us>
2016-07-02 01:03:58 -0400

    Add charlist package

---
+++ src/apemanzilla/charlist.lua
@@ -0,0 +1,10 @@
+local stringutils = require "stringutils"
+local pager = require "pager"
+
+local str = ""
+
+for i = 32, 255 do
+	str = str .. stringutils.fixLength(i .. ": " .. string.char(i), 6) .. ((i % 7 == 0 and "\n") or " ")
+end
+
+pager.scroll(str .. "\n\n")
